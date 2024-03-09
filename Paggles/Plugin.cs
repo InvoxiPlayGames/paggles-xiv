6eb904134cdf4046eefebab086e779fd78ceecd4
InvoxiPlayGames <webmaster@invoxiplaygames.uk>
2024-03-09 18:13:59 +0000

    lol

---
+++ Paggles/Plugin.cs
@@ -0,0 +1,58 @@
+using Dalamud.Game.Command;
+using Dalamud.IoC;
+using Dalamud.Plugin;
+using System.IO;
+using Dalamud.Interface.Windowing;
+using Dalamud.Plugin.Services;
+using Dalamud.Game.Addon.Lifecycle;
+using Dalamud.Game.Addon.Lifecycle.AddonArgTypes;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace Paggles
+{
+    public sealed class Plugin : IDalamudPlugin
+    {
+        private IAddonLifecycle AddonLifecycle { get; init; }
+
+        private float? original = null;
+
+        public Plugin(IAddonLifecycle addonLifecycle)
+        {
+            this.AddonLifecycle = addonLifecycle;
+
+            AddonLifecycle.RegisterListener(AddonEvent.PreSetup, "_DTR", ResetPaggles);
+            AddonLifecycle.RegisterListener(AddonEvent.PostRequestedUpdate, "_DTR", PagglesTime);
+        }
+
+        public void Dispose()
+        {
+            AddonLifecycle.UnregisterListener(ResetPaggles);
+            AddonLifecycle.UnregisterListener(PagglesTime);
+        }
+
+        public void ResetPaggles(AddonEvent type, AddonArgs args)
+        {
+            original = null;
+        }
+
+        public unsafe void PagglesTime(AddonEvent type, AddonArgs args)
+        {
+            AtkUnitBase* dtrButtonBase = (AtkUnitBase*)args.Addon;
+            AtkComponentButton* dtrTimeButton = dtrButtonBase->GetButtonNodeById(16);
+            AtkTextNode *textNode = dtrTimeButton->ButtonTextNode;
+
+            string originalText = textNode->NodeText.ToString();
+            originalText = originalText.Replace("a.m.", "aggles");
+            originalText = originalText.Replace("p.m.", "paggles");
+
+            textNode->SetText(originalText);
+            textNode->ResizeNodeForCurrentText();
+
+            if (original is null)
+                original = dtrButtonBase->RootNode->X;
+
+            float diff = dtrButtonBase->RootNode->Width - textNode->AtkResNode.Width;
+            dtrButtonBase->RootNode->X = (float)(original + diff);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# paggles
+
+My Final Fantasy XIV says paggles right next to the time, what does this mean?
